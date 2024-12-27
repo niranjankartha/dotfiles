657f12eb089a71e018c32e87cffa1e13e99b6e4c
Niranjan A. Kartha <niranjankartha@outlook.com>
2024-12-27 20:38:47 +0530

    Improve updater script

--- update.py
+++ update.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python
-
 import os
 import shutil
 
-HOME = os.getenv("HOME")
+path_map = {
+    ".config/hypr": "~/.config/hypr",
+    ".config/my": "~/.config/my",
+    ".config/waybar": "~/.config/waybar",
+    ".config/zed": "~/.config/zed",
+    "bin": "~/bin",
+    ".zshrc": "~/.zshrc",
+};
 
-for dir in os.listdir("config"):
-    shutil.copytree(f"{HOME}/.config/{dir}", f"config/{dir}", dirs_exist_ok=True)
+def copy_if_newer(src: str, dst: str):
+    dst_t = os.path.getmtime(dst) if os.path.exists(dst) else 0
+    src_t = os.path.getmtime(src)
+    if src_t <= dst_t:
+        return
+    if os.path.isfile(src):
+        shutil.copyfile(src, dst)
+        return
 
-for file in os.listdir("bin"):
-    shutil.copyfile(f"{HOME}/bin/{file}", f"bin/{file}")
+    for child in os.listdir(src):
+        copy_if_newer(f"{src}/{child}", f"{dst}/{child}")
+
+    for child in os.listdir(dst):
+        if os.path.exists(f"{src}/{child}"):
+            continue
+        dst_child = f"{dst}/{child}"
+        if os.path.isfile(dst_child):
+            os.remove(dst_child)
+        else:
+            shutil.rmtree(dst_child)
+
+HOME = os.getenv("HOME") or "/home/arch"
+for dst in path_map:
+    copy_if_newer(path_map[dst].replace("~", HOME), dst)
